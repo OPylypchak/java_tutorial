86609f73910fc210aa19b912d7da5d0e2fdf891e
OPylypchak <olya-ch777@yandex.ru>
2016-02-28 22:13:48 +0200

    created function distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,22 +2,31 @@ package ru.stqa.pft.sandbox;
 
 import com.sun.org.apache.regexp.internal.RE;
 
+import static java.lang.Math.*;
+
 public class MyFirstProgram {
 
  public static void main(String[] args) {
-  hello("world");
-  hello("user");
-  hello("Olga");
 
-  Square s = new Square(5);
+   double x1 = 5;
+   double y1 = 6;
+   double x2 = 1;
+   double y2 = 2;
+
+   Point p1 = new Point(x1, y1);
+   Point p2 = new Point(x2, y2);
+
+  System.out.println("Расстояние между точками p1(" + x1 + "; " + y1 + ") и p2 (" + x2 + "; " + y2 + ") = " + distance(p1, p2));
+
+  /*Square s = new Square(5);
   System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
   Rectangle r = new Rectangle(4, 6);
-  System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+  System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());*/
+
  }
 
- public static void hello(String somebody) {
-   System.out.println("Hello, " + somebody + "!");
- }
-
+  private static double distance(Point p1, Point p2){
+    return sqrt((p2.x - p1.x)*(p2.x - p1.x)+(p2.y -p1.y)*(p2.y - p1.y));
+  }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Оля on 28.02.2016.
+ */
+public class Point {
+
+  public double x;
+  public double y;
+
+public Point (double x, double y){
+  this.x = x;
+  this.y = y;
+}
+
+
+}
